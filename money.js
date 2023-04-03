dec33fd3068908e08d172d6448fee81a43337253
Edgar Hovsepyan <128087447+EdgarHovsepyan17@users.noreply.github.com>
2023-04-03 21:45:36 +0400

    Add files via upload

---
+++ money.js
@@ -0,0 +1,47 @@
+jQuery("document").ready(function () {
+
+	jQuery('button').on("click", function () {
+
+		var gin, tiv, fob,usgin;
+		gin = jQuery("#val1").val()
+		tiv = jQuery("#val2").val()
+
+		gin = parseInt(gin);
+		tiv = parseInt(tiv);
+
+		if (gin <= 999) {
+			fob = 200
+		}
+		else if (gin <= 1999) {
+			fob = 350
+		}
+		else if (gin <= 3499) {
+			fob = 500
+		}
+		else if (gin <= 4999) {
+			fob = 750
+		}
+		else if (gin <= 6999) {
+			fob = 890
+		}
+		else if (gin <= 14999) {
+			fob = 1000
+		}
+		else {
+			fob=gin*0.175
+		}
+
+		usgin=gin+fob+2500
+
+		if (tiv <= 2019) {
+			usgin = usgin * 1.45
+		}
+		else {
+			usgin=usgin*1.38
+		}
+
+		jQuery('#rez').html(usgin);
+	});
+});
+
+
